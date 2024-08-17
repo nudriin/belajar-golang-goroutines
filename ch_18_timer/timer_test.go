f20b88b35daa2ad2a86eaabf8226665720607b55
Nurdin <mrsunnysummer@gmail.com>
2024-08-17 22:01:16 +0700

    Chapter 18 timer

---
+++ ch_18_timer/timer_test.go
@@ -0,0 +1,39 @@
+package ch_18_timer
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestTimer(t *testing.T) {
+	timer := time.NewTimer(5 * time.Second)
+	fmt.Println(time.Now()) // di print duluan
+
+	time := <-timer.C
+	fmt.Println(time) // data akan masuk setelah 5 detik
+}
+
+func TestAfter(t *testing.T) {
+	channel := time.After(5 * time.Second)
+	fmt.Println(time.Now()) // di print duluan
+
+	time := <-channel
+	fmt.Println(time) // data akan dikirim setelah 5 detik
+
+}
+
+func TestAfterFunction(t *testing.T) {
+	var group sync.WaitGroup
+	group.Add(1)
+
+	time.AfterFunc(5*time.Second, func() {
+		fmt.Println(time.Now())
+		fmt.Println("Execute after 5 second")
+		group.Done()
+	})
+	fmt.Println(time.Now()) // akan di print dulu
+
+	group.Wait()
+}
